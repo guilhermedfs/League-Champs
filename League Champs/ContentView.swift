9f0e85cd233fa03299ae5a6a96b227b836bc7522
Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>
2022-01-24 17:21:35 -0300

    Initial Commit

---
+++ League Champs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  League Champs
+//
+//  Created by Guilherme - ília on 24/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ League Champs/League_ChampsApp.swift
@@ -0,0 +1,17 @@
+//
+//  League_ChampsApp.swift
+//  League Champs
+//
+//  Created by Guilherme - ília on 24/01/22.
+//
+
+import SwiftUI
+
+@main
+struct League_ChampsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
